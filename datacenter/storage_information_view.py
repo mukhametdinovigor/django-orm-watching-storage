c37643170f31112bf9dc84e6054263abab6d6c2b
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-02-12 07:20:14 +1100

    Delete comments

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,4 +1,3 @@
-from datacenter.models import Passcard
 from datacenter.models import Visit
 from datacenter.models import get_duration
 from django.shortcuts import render
@@ -23,6 +22,6 @@ def storage_information_view(request):
             }
         )
     context = {
-            "non_closed_visits": non_closed_visits,  # не закрытые посещения
+            "non_closed_visits": non_closed_visits
         }
     return render(request, 'storage_information.html', context)
